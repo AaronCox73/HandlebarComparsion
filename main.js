1eea7d699e84268666410433c474b29452b70fb8
AJ Cox <ajcox@ajs-macbook-pro.lan>
2022-11-30 21:34:35 -0700

    figuring out how to post tr and td to DOM

---
+++ main.js
@@ -0,0 +1,867 @@
+let protaper = [
+    {
+        name: 'Carmichael',
+        width: 800,
+        height: 77,
+        sweep: 55
+    },
+    {
+        name: 'Henry / Reed',
+        width: 800,
+        height: 92,
+        sweep: 57
+    },
+    {
+        name: 'Pastrana MX/RM Low',
+        width: 800,
+        height: 74,
+        sweep: 55
+    },
+    {
+        name: 'Pastrana FMX',
+        width: 800,
+        height: 121,
+        sweep: 55
+    },
+    {
+        name: 'Windham/RM Mid',
+        width: 800,
+        height: 99,
+        sweep: 53
+    },
+    {
+        name: 'Factory Suzuki/KTM Stock',
+        width: 800,
+        height: 92,
+        sweep: 47
+    },
+    {
+        name: 'Woods Low',
+        width: 800,
+        height: 71,
+        sweep: 60
+    },
+    {
+        name: 'Woods High',
+        width: 800,
+        height: 95,
+        sweep: 57
+    },
+    {
+        name: 'CR Mid',
+        width: 800,
+        height: 82,
+        sweep: 57
+    },
+    {
+        name: 'CR High',
+        width: 800,
+        height: 95,
+        sweep: 60
+    },
+    {
+        name: 'KX High',
+        width: 800,
+        height: 108,
+        sweep: 56
+    },
+    {
+        name: 'YZ High',
+        width: 800,
+        height: 84,
+        sweep: 52
+    },
+    {
+        name: 'Universal Low',
+        width: 800,
+        height: 87,
+        sweep: 54
+    }
+]
+
+let renthal = [
+
+    {
+        name: 'YAM YZ 07-23 / YZF 07-17',
+        number: 602,
+        width: 801,
+        height: 88,
+        sweep: 56,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'Reed/Windham',
+        number: 603,
+        width: 800,
+        height: 98,
+        sweep: 55,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'RC / HON CRF 04-18 KAW KX/KXF 2006-20',
+        number: 604,
+        width: 804,
+        height: 104,
+        sweep: 54,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'RICKY JOHNSON / CR HIGH / KTM ENDURO 2017-18',
+        number: 605,
+        width: 805,
+        height: 97,
+        sweep: 65,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'RC HIGH',
+        number: 609,
+        width: 802,
+        height: 120,
+        sweep: 56,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'ENDURO HIGH',
+        number: 613,
+        width: 835,
+        height: 135,
+        sweep: 78,
+        type: '7/8th'
+    },
+
+    {
+        name: '5.0 IN TRIALS',
+        number: 660,
+        width: 817,
+        height: 109,
+        sweep: 49,
+        type: '7/8th'
+    },
+
+    {
+        name: 'Enduro Medium',
+        number: 664,
+        width: 787,
+        height: 79,
+        sweep: 75,
+        type: '7/8th'
+    },
+
+    {
+        name: 'Vintage/Desert',
+        number: 666,
+        width: 850,
+        height: 119,
+        sweep: 74,
+        type: '7/8th'
+    },
+
+    {
+        name: 'KTM SX/SX-F 09-12 SUZI RM/RMZ 06-13 YAM YZ-F 18-21',
+        number: 672,
+        width: 806,
+        height: 85,
+        sweep: 56,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'TRIALS 100 Bou/Lampkin',
+        number: 673,
+        width: 819,
+        height: 100,
+        sweep: 47,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'ENDURO',
+        number: 693,
+        width: 794,
+        height: 101,
+        sweep: 73,
+        type: '7/8th'
+    },
+
+    {
+        name: 'LARRY ROESELER / BAJA RACER',
+        number: 699,
+        width: 784,
+        height: 98,
+        sweep: 81,
+        type: '7/8th'
+    },
+
+    {
+        name: 'HONDA CRF150/230 2016-',
+        number: 701,
+        width: 793,
+        height: 83,
+        sweep: 73,
+        type: '7/8th'
+    },
+
+    {
+        name: 'CR HIGH / Ricky Johnson',
+        number: 722,
+        width: 800,
+        height: 95,
+        sweep: 63,
+        type: '7/8th'
+    },
+
+    {
+        name: '5.5 in trials',
+        number: 735,
+        width: 817,
+        height: 122,
+        sweep: 49,
+        type: '7/8th'
+    },
+
+    {
+        name: '4.5 in trials',
+        number: 742,
+        width: 823,
+        height: 104,
+        sweep: 50,
+        type: '7/8th'
+    },
+
+    {
+        name: 'Enduro',
+        number: 745,
+        width: 781,
+        height: 102,
+        sweep: 54,
+        type: 'Fatbar'
+    },
+
+    {
+        name: 'SUZUKI RM125/250 96-01',
+        number: 759,
+        width: 805,
+        height: 72,
+        sweep: 65,
+        type: '7/8th'
+    },
+
+    {
+        name: 'JIMMY BUTTON',
+        number: 764,
+        width: 814,
+        height: 111,
+        sweep: 68,
+        type: '7/8th'
+    },
+
+    {
+        name: '6.0 in trials',
+        number: 768,
+        width: 828,
+        height: 126,
+        sweep: 51,
+        type: '7/8th'
+    },
+    {
+        name: 'CR Mid',
+        number: 773,
+        width: 801,
+        height: 88,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: 'SUZUKI RM 2004 & KAWASAKI KX',
+        number: 779,
+        width: 803,
+        height: 75,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: 'CR LOW',
+        number: 790,
+        width: 803,
+        height: 82,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: 'RC HIGH',
+        number: 809,
+        width: 814,
+        height: 119,
+        sweep: 56,
+        type: '7/8th'
+    },
+    {
+        name: 'CRF150R ADULT BAR',
+        number: 816,
+        width: 783,
+        height: 137,
+        sweep: 44,
+        type: '7/8th'
+    },
+    {
+        name: 'MCGRATH / KTM SX125-450 2016+ / Suzuki 2018+',
+        number: 821,
+        width: 813,
+        height: 78,
+        sweep: 54,
+        type: 'Fatbar'
+    },
+    {
+        name: 'KTM LOW',
+        number: 822,
+        width: 808,
+        height: 83,
+        sweep: 41,
+        type: 'Fatbar'
+    },
+    {
+        name: 'KTM HIGH / HUSQ ENDURO 2018-21',
+        number: 826,
+        width: 805,
+        height: 95,
+        sweep: 42,
+        type: 'Fatbar'
+    },
+    {
+        name: 'VILLOPOTO/STEWART/KTM 2011-16',
+        number: 827,
+        width: 811,
+        height: 92,
+        sweep: 52,
+        type: 'Fatbar'
+    },
+    {
+        name: 'TRIALS 110',
+        number: 829,
+        width: 816,
+        height: 115,
+        sweep: 45,
+        type: 'Fatbar'
+    },
+    {
+        name: 'VINTAGE MID',
+        number: 837,
+        width: 874,
+        height: 145,
+        sweep: 120,
+        type: '7/8th'
+    },
+    {
+        name: 'VINTAGE HIGH',
+        number: 838,
+        width: 840,
+        height: 155,
+        sweep: 88,
+        type: '7/8th'
+    },
+    {
+        name: ' RICKY JOHNSON / CR HIGH',
+        number: 918,
+        width: 801,
+        height: 99,
+        sweep: 65,
+        type: 'Twinwall'
+    },
+    {
+        name: 'YAM YZ/YZF 06',
+        number: 921,
+        width: 816,
+        height: 85,
+        sweep: 41,
+        type: 'Twinwall'
+    },
+    {
+        name: 'RC HIGH',
+        number: 922,
+        width: 812,
+        height: 120,
+        sweep: 58,
+        type: 'Twinwall'
+    },
+    {
+        name: 'YZ/WR 1997-2004',
+        number: 966,
+        width: 807,
+        height: 88,
+        sweep: 60,
+        type: '7/8th'
+    },
+    {
+        name: 'RC',
+        number: 971,
+        width: 802,
+        height: 97,
+        sweep: 53,
+        type: '7/8th'
+    },
+    {
+        name: 'VILLOPOTO / STEWART',
+        number: 983,
+        width: 808,
+        height: 95,
+        sweep: 55,
+        type: '7/8th'
+    },
+    {
+        name: 'KTM HIGH',
+        number: 994,
+        width: 810,
+        height: 99,
+        sweep: 40,
+        type: 'Twinwall'
+    },
+    {
+        name: 'VILLOPOTO/STEWART / HON CRF 2019+',
+        number: 996,
+        width: 811,
+        height: 93,
+        sweep: 49,
+        type: 'Twinwall'
+    },
+    {
+        name: 'RC / HON CRF 04-18 KAW KX/KXF 2006+',
+        number: 997,
+        width: 804,
+        height: 105,
+        sweep: 54,
+        type: 'Twinwall'
+    },
+    {
+        name: 'REED / WINDHAM',
+        number: 998,
+        width: 803,
+        height: 98,
+        sweep: 56,
+        type: 'Twinwall'
+    },
+    {
+        name: 'MCGRATH / KTM SX125-450 2016+',
+        number: 999,
+        width: 813,
+        height: 79,
+        sweep: 54,
+        type: 'Twinwall'
+    }
+
+]
+
+// let neken = [
+//     {
+//         name: ,
+//         width: ,
+//         height: ,
+//         sweep: 
+//     },
+
+// ]
+
+let tusk = [
+    {
+        name: 'CR High Bend',
+        width: 800,
+        height: 121,
+        sweep: 61,
+        type: 'Chub'
+    },
+    {
+        name: 'KTM Bend',
+        width: 811,
+        height: 92,
+        sweep: 52,
+        type: 'Chub'
+    },
+    {
+        name: 'Moto Bend',
+        width: 800,
+        height: 99,
+        sweep: 54,
+        type: 'Chub'
+    },
+    {
+        name: 'YZ Bend',
+        width: 803,
+        height: 89,
+        sweep: 60,
+        type: 'Chub'
+    },
+    {
+        name: 'CR High Bend',
+        width: 813,
+        height: 95,
+        sweep: 76,
+        type: '7/8th'
+    },
+    {
+        name: 'CR Mid Bend',
+        width: 813,
+        height: 89,
+        sweep: 63.5,
+        type: '7/8th'
+    },
+    {
+        name: 'KX High Bend',
+        width: 819,
+        height: 102,
+        sweep: 76,
+        type: '7/8th'
+    },
+    {
+        name: 'YZ High Bend',
+        width: 813,
+        height: 89,
+        sweep: 76,
+        type: '7/8th'
+    }
+
+]
+
+let phoneix = [
+    {
+        name: '118 70',
+        width: 800,
+        height: 70,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: '118 80',
+        width: 800,
+        height: 80,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: '118 90',
+        width: 800,
+        height: 90,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: '118 100',
+        width: 800,
+        height: 100,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: '78 70',
+        width: 800,
+        height: 70,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: '78 80',
+        width: 800,
+        height: 80,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: '78 90',
+        width: 800,
+        height: 90,
+        sweep: 57,
+        type: '7/8th'
+    },
+    {
+        name: '78 100',
+        width: 800,
+        height: 100,
+        sweep: 57,
+        type: '7/8th'
+    },
+
+]
+
+let astra = [
+    {
+        name: 'Apollo 30',
+        width: 804,
+        height: 30,
+        sweep: 48
+    },
+    {
+        name: 'Apollo 35',
+        width: 806,
+        height: 35,
+        sweep: 48
+    },
+    {
+        name: 'Gemini 27',
+        width: 804,
+        height: 27,
+        sweep: 50
+    },
+    {
+        name: 'Gemini 33',
+        width: 806,
+        height: 33,
+        sweep: 50
+    },
+    {
+        name: 'Osiris 35',
+        width: 808,
+        height: 35,
+        sweep: 52
+    },
+    {
+        name: 'Osiris 42',
+        width: 810,
+        height: 42,
+        sweep: 52
+    },
+    {
+        name: 'Auroa 32',
+        width: 812,
+        height: 32,
+        sweep: 49
+    },
+    {
+        name: 'Auroa 37',
+        width: 814,
+        height: 37,
+        sweep: 49
+    },
+    {
+        name: 'Auroa 43',
+        width: 816,
+        height: 43,
+        sweep: 49
+    },
+    {
+        name: 'Athena 50',
+        width: 810,
+        height: 50,
+        sweep: 50
+    },
+
+]
+
+let ODI = [
+    {
+        name: 'HON / KAW OE',
+        width: 802,
+        height: 99,
+        sweep: 54,
+        type: '7/8th'
+    },
+    {
+        name: 'McGRATH',
+        width: 805,
+        height: 77,
+        sweep: 55,
+        type: '7/8th'
+    },
+    {
+        name: 'CHAMP',
+        width: 805,
+        height: 93,
+        sweep: 55,
+        type: '7/8th'
+    },
+    {
+        name: 'KTM OE',
+        width: 805,
+        height: 95,
+        sweep: 42,
+        type: '7/8th'
+    },
+    {
+        name: 'RC-HIGH',
+        width: 805,
+        height: 120,
+        sweep: 54,
+        type: '7/8th'
+    },
+    {
+        name: 'HON / KAW OE',
+        width: 802,
+        height: 99,
+        sweep: 54,
+        type: '1-1/8th'
+    },
+    {
+        name: 'McGRATH',
+        width: 805,
+        height: 77,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: 'RC4 SIGNATURE',
+        width: 805,
+        height: 78,
+        sweep: 54,
+        type: '1-1/8th'
+    },
+    {
+        name: 'CHAMP',
+        width: 805,
+        height: 93,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: 'COUNTRY BOY',
+        width: 803,
+        height: 92,
+        sweep: 57,
+        type: '1-1/8th'
+    },
+    {
+        name: 'KTM OE',
+        width: 805,
+        height: 95,
+        sweep: 42,
+        type: '1-1/8th'
+    },
+    {
+        name: 'KTM 16-18',
+        width: 805,
+        height: 87,
+        sweep: 48,
+        type: '1-1/8th'
+    },
+    {
+        name: 'CR HIGH',
+        width: 805,
+        height: 95,
+        sweep: 62,
+        type: '1-1/8th'
+    },
+    {
+        name: 'RC HIGH',
+        width: 805,
+        height: 120,
+        sweep: 54,
+        type: '1-1/8th'
+    },
+    {
+        name: 'FMX',
+        width: 800,
+        height: 121,
+        sweep: 55,
+        type: '1-1/8th'
+    },
+    {
+        name: 'YZ OE',
+        width: 802,
+        height: 90,
+        sweep: 52,
+        type: '1-1/8th'
+    },
+
+
+]
+
+let mika = [
+    {
+        name: 'YZ / Reed',
+        width: 803,
+        height: 92,
+        sweep: 57
+    },
+    {
+        name: 'Stewart / Villopoto (996)',
+        width: 805,
+        height: 93,
+        sweep: 55
+    },
+    {
+        name: 'RC BEND/HONDA STOCK/KAW STOCK (997)',
+        width: 802,
+        height: 99,
+        sweep: 56
+    },
+    {
+        name: 'CR HIGH BEND (918)',
+        width: 795,
+        height: 95,
+        sweep: 57
+    },
+    {
+        name: 'CR LOW BEND',
+        width: 802,
+        height: 66,
+        sweep: 57
+    },
+    {
+        name: 'MC BEND (999)',
+        width: 805,
+        height: 85,
+        sweep: 55
+    },
+    {
+        name: 'KTM BEND',
+        width: 805,
+        height: 90,
+        sweep: 50
+    },
+    {
+        name: 'SX BEND (stock husky)',
+        width: 810,
+        height: 80,
+        sweep: 51
+    },
+    {
+        name: 'BBT (Big Bike Tall)',
+        width: 812,
+        height: 120,
+        sweep: 57
+    }
+]
+
+
+
+let tr = document.createElement('tr')
+let td = document.createElement('td')
+
+
+// forEach and for in loop to try
+// mika.forEach(handlebar => {
+//     for (let key in handlebar) {
+//         console.log(tr.innerHTML = (`${handlebar[key]}`))
+//         // console.log(`${key}: ${handlebar[key]}`)
+//         // console.log(td.innerHTML = (handlebar[key]))
+//         document.body.append(tr.cloneNode(true))
+//     }
+// })
+
+
+
+// for loop to try
+// for (let i = 0; i < protaper.length; i++) {
+//     li.innerHTML = (JSON.stringify(protaper[i]))
+//     document.body.appendChild(li.cloneNode(true))
+//     console.log(protaper[i])
+// }
+
+// console.log('--------------------------------')
+
+// // foreach method
+// phoneix.forEach(handlebar => console.log(handlebar))
+
+// console.log('--------------------------------')
+
+
+// for of loop to try 
+for (let handlebar of tusk) {
+    console.log(td.innerHTML = `${handlebar.name}`)
+    // document.body.appendChild(td.cloneNode(true))
+    console.log((JSON.stringify(handlebar.width)))
+    console.log((JSON.stringify(handlebar.height)))
+    console.log((JSON.stringify(handlebar.sweep)))
+}
