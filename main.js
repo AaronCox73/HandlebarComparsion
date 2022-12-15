cafc580a9d6d483b657f83b0328b70ffc0a51005
AJ Cox <ajcox@ajs-macbook-pro.lan>
2022-12-14 20:38:10 -0700

    added drop down button for filter

--- main.js
+++ main.js
@@ -963,7 +963,7 @@ function produceTable2(handlebarBrand) {
 filterBtn.addEventListener('click', function () { produceTable(handlebars) })
 
 
-// SORT BUTTONS
+// SORT/filter BUTTONS
 
 // sort by width per table. 
 sortByAlphaBtn.addEventListener('click', function () {
@@ -995,13 +995,13 @@ sortBySweepBtn.addEventListener('click', function () {
 
 })
 
-produceTable2(mika)
-produceTable2(astra)
-produceTable2(protaper)
-produceTable2(renthal)
-produceTable2(tusk)
-produceTable2(phoneix)
-produceTable2(ODI)
+// produceTable2(mika)
+// produceTable2(astra)
+// produceTable2(protaper)
+// produceTable2(renthal)
+// produceTable2(tusk)
+// produceTable2(phoneix)
+// produceTable2(ODI)
 
 
 
